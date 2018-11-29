e8656bac0b23f6177e6801c0c34bed94128f7a80
Yan_dellwork <pkmn99@gmail.com>
2018-11-29 10:25:06 +0800

    correct co2 effect calculation and updates for tomatoes

--- code/process_yield_prediction.py
+++ code/process_yield_prediction.py
@@ -26,10 +26,10 @@ def yield_trend(df, order=1):
 # Train the model, specify gs months 
 def train_model(m,form='poly',crop='potatoes'):    
     model_txt = "Q('yield_ana') ~ " + predictor_constructor('tmax_p',1,m,form) + predictor_constructor('tmin_p',1,m,form)
-#    model_txt = "Q('yield_ana') ~ " + predictor_constructor('tmax_p',1,m,form)
+#    model_txt = "Q('yield_ana') ~ " + predictor_constructor('tmax_p',1,3,form)
     
     # Load data
-    d_model = pd.read_csv('../data/%s_model_data_2016_update.csv'%crop,dtype={'FIPS':str})
+    d_model = pd.read_csv('../data/%s_model_data_update.csv'%crop,dtype={'FIPS':str})
     
     m = yield_trend(d_model, order=2)
     d_model['yield_ana'] = d_model['yield'] - m.predict(d_model)
@@ -38,7 +38,11 @@ def train_model(m,form='poly',crop='potatoes'):
 
 # Use the trained model to predict yield (function)
 def get_yield_prediction(crop='potatoes',name='historical'):
-    yield_unit_func = lambda x: x*100*0.45359237/0.404686 * 0.2 /1000 # cwt/acre to dry matter t/ha, 80% moisture
+    if crop=='potatoes':
+        yield_unit_func = lambda x: x* 0.112085 * 0.2 #*100*0.45359237/0.404686 /1000 # cwt/acre to dry matter t/ha, 80% moisture
+    if crop == 'tomatoes':
+        yield_unit_func = lambda x: x/0.404686 * 0.06 # ton/acre to dry matter t/ha, 96% moisture
+
     d4 = pd.read_csv('../data/Weather/monthly_climate_%s_model_gs_%s_m%d.csv'%(name,crop,4))
     d5 = pd.read_csv('../data/Weather/monthly_climate_%s_model_gs_%s_m%d.csv'%(name,crop,5))
     d6 = pd.read_csv('../data/Weather/monthly_climate_%s_model_gs_%s_m%d.csv'%(name,crop,6))
@@ -71,30 +75,32 @@ def save_latlon_county_link():
     return 
 
 # Load the potatoes baseline yield data
-def get_yield_obs():
-    y_hist_obs = pd.read_csv('../data/county_namelist32.csv',header=1,index_col=[0])
+def get_yield_baseline(crop='potatoes'):
+    y_base = pd.read_csv('../data/%s_baseline_yield.csv'%crop,index_col=[0])
     llc = pd.read_csv('../data/latlon_county_link.csv')
-    return y_hist_obs[['State','County','Corrected Tuber Dry Weight (t/ha) (for calibration)']] \
-          .merge(llc,on='County').rename(columns={'Corrected Tuber Dry Weight (t/ha) (for calibration)':'yield_base'})
+    return y_base[['State','County','Baseline Yield (t/ha)']] \
+          .merge(llc,on='County').rename(columns={'Baseline Yield (t/ha)':'yield_base'})
 
 # Calculate the co2 effect on potatoes yield
-def get_co2_effect():
+def get_co2_effect(crop='potatoes'): # the output should be ton/ha dry matter
     co2 = pd.read_csv('../data/co2_1981-2070.csv')
-    co2_func = lambda x: 83.5 + 0.275 * x # equation from literature
-    yield_unit_func = lambda x: x*100*0.45359237/0.404686 * 0.2 /1000 # cwt/acre to dry matter t/ha, 80% moisture
-    co2['co2_yield_effect'] = yield_unit_func(co2_func(co2['co2']) - co2_func(340))
+    if crop == 'potatoes':
+        co2_func = lambda x: 83.5 + 0.275 * x # g/plant equation from literature
+        # cwt hundred pound
+        yield_unit_func = lambda x: x * 0.15 # assume 15 plant per m2
+    if crop == 'tomatoes':
+        co2_func = lambda x: 0.265 * x # equation from literature, ton/ha
+        yield_unit_func = lambda x: x * 0.06 # convert to dry matter t/ha, 94% moisture
+    # CO2 basline is 360 ppm for non-co2     
+    co2['co2_yield_effect'] = yield_unit_func(co2_func(co2['co2']) - co2_func(360)) 
     return co2
 
 # Use stat model to predict historical and future yield
 def get_prediction_result(crop='potatoes',name='historical'):
-    y_hist_obs = get_yield_obs()
+    y_baseline = get_yield_baseline(crop=crop)
     d = get_yield_prediction(crop=crop, name=name)
-    co2 = get_co2_effect()
-   # if name == 'historical':
-   #     y = y_hist_obs.merge(d, on='location')
-   # else:    
-   #     y = y_hist_obs.merge(d, on='location').merge(co2,on='year')
-    y = y_hist_obs.merge(d, on='location').merge(co2,on='year')
+    co2 = get_co2_effect(crop=crop)
+    y = y_baseline.merge(d, on='location').merge(co2,on='year')
     y.loc[:,'yield_predicted_withco2'] = y.loc[:,'yield_base'] + y.loc[:,'yield_ana_predicted'] + y.loc[:,'co2_yield_effect']
     y.loc[:,'yield_predicted'] = y.loc[:,'yield_base'] + y.loc[:,'yield_ana_predicted']
     return y       
@@ -120,13 +126,13 @@ def load_prediction_result(name,crop='potatoes'):
     return t
 
 # Save the predicition results to the template excel format (need to mannual copy)
-def save_excel_format():
+def save_excel_format(crop='potatoes'):
     # Without Adaptation 
-    d1 = load_prediction_result('GFDL-ESM2M').set_index(['State','County','year'])
-    d2 = load_prediction_result('HadGEM2-ES').set_index(['State','County','year'])
-    d3 = load_prediction_result('IPSL-CM5A-LR').set_index(['State','County','year'])
-    d4 = load_prediction_result('MIROC-ESM-CHEM').set_index(['State','County','year'])
-    d5 = load_prediction_result('NorESM1-M').set_index(['State','County','year'])
+    d1 = load_prediction_result('GFDL-ESM2M',crop=crop).set_index(['State','County','year'])
+    d2 = load_prediction_result('HadGEM2-ES',crop=crop).set_index(['State','County','year'])
+    d3 = load_prediction_result('IPSL-CM5A-LR',crop=crop).set_index(['State','County','year'])
+    d4 = load_prediction_result('MIROC-ESM-CHEM',crop=crop).set_index(['State','County','year'])
+    d5 = load_prediction_result('NorESM1-M',crop=crop).set_index(['State','County','year'])
 
     d = pd.concat([d1, d2,d3,d4,d5], axis=1, 
               keys=['GFDL-ESM2M', 'HadGEM2-ES', 'IPSL-CM5A-LR', 'MIROC-ESM-CHEM', 'NorESM1-M'])
@@ -138,11 +144,11 @@ def save_excel_format():
     d.loc[(slice(None),slice(None),slice(2041,2070)),(slice(None),'yield_predicted_withco2')].to_csv('../data/result/block_2050s_noada_co2.csv')
     
     # With Adaptation 
-    d1a = load_prediction_result('GFDL-ESM2M_adaptation').set_index(['State','County','year'])
-    d2a = load_prediction_result('HadGEM2-ES_adaptation').set_index(['State','County','year'])
-    d3a = load_prediction_result('IPSL-CM5A-LR_adaptation').set_index(['State','County','year'])
-    d4a = load_prediction_result('MIROC-ESM-CHEM_adaptation').set_index(['State','County','year'])
-    d5a = load_prediction_result('NorESM1-M_adaptation').set_index(['State','County','year'])
+    d1a = load_prediction_result('GFDL-ESM2M_adaptation',crop=crop).set_index(['State','County','year'])
+    d2a = load_prediction_result('HadGEM2-ES_adaptation',crop=crop).set_index(['State','County','year'])
+    d3a = load_prediction_result('IPSL-CM5A-LR_adaptation',crop=crop).set_index(['State','County','year'])
+    d4a = load_prediction_result('MIROC-ESM-CHEM_adaptation',crop=crop).set_index(['State','County','year'])
+    d5a = load_prediction_result('NorESM1-M_adaptation',crop=crop).set_index(['State','County','year'])
 
     da = pd.concat([d1a, d2a,d3a,d4a,d5a], axis=1, 
               keys=['GFDL-ESM2M', 'HadGEM2-ES', 'IPSL-CM5A-LR', 'MIROC-ESM-CHEM', 'NorESM1-M'])
@@ -152,5 +158,6 @@ def save_excel_format():
     print('All excel blocks saved')        
 
 if __name__ == "__main__":
-    save_prediction_result()
-#    save_excel_format()
+#    save_prediction_result(crop='tomatoes')
+#    save_prediction_result(crop='potatoes')
+    save_excel_format(crop='tomatoes')
